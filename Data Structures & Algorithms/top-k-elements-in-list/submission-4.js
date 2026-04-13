a18751322360077d6065ef20c21fe7996b28ebcd
Nevin Edwin <71591537+nevinedwin@users.noreply.github.com>
2026-04-13 10:40:55 +0530

    Add: top-k-elements-in-list - submission-4

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const freqMap = {};
+        const bucket = Array.from({length: nums.length + 1}, () => []);
+
+        for (let i =0; i< nums.length; i++){
+            freqMap[nums[i]] = (freqMap[nums[i]] || 0) + 1;
+        };
+
+        Object.entries(freqMap).forEach(([num, freq]) => {
+            bucket[freq].push(num);
+        });
+
+        const result = [];
+        for (let j = bucket.length - 1; j >= 0 && result.length !== k; j--){
+            for (const num of bucket[j]){
+                result.push(num);
+                if (result.length === k) return result;
+            }
+        }
+    };
+}
